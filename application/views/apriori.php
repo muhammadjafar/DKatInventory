54e1d0e4ce7af33b5053a6bf13ea48eacac1cd82
Ahmad Syamsul Arifin <aarie33ahmad@gmail.com>
2018-11-11 07:15:02 +0700

    with loading

--- application/views/apriori.php
+++ application/views/apriori.php
@@ -1,3 +1,18 @@
+        <style>
+          #ctn_loading {
+              border: 8px solid #f3f3f3; /* Light grey */
+              border-top: 8px solid #3498db; /* Blue */
+              border-radius: 50%;
+              width: 70px;
+              height: 70px;
+              animation: spin 2s linear infinite;
+          }
+
+          @keyframes spin {
+              0% { transform: rotate(0deg); }
+              100% { transform: rotate(360deg); }
+          }
+        </style>
         <div class="my-3 my-md-5">
           <div class="container">
             <div class="page-header">
@@ -5,41 +20,56 @@
                 Analisa rekomendasi lokasi peletakan barang
               </h1>
             </div>
-            <div class="row row-cards">
-              <?php for($i=1; $i<rand(2,9); $i++){ ?>
-                <div class="col-6 col-sm-6 col-lg-3">
-                  <div class="card border border-primary" style="background: rgba(3, 169, 244, 0.1);">
-                    <div class="card-body p-3 text-center">
-                      <!-- <div class="text-right text-green">
-                        6%
-                        <i class="fe fe-chevron-up"></i>
-                      </div> -->
-                      <div class="row">
-                        <div class="col-md-4">
-                          <div class="h1 m-0"><i class="fa fa-box"></i></div>
-                          <div class="text-muted mb-4"><?php echo $barang[$i*10];?></div>
+            <center><div id="ctn_loading"></div></center>
+            <div class="row" id="ctn_analisa">
+                <?php for($i=1; $i<rand(2,9); $i++){ ?>
+                  <div class="col-6 col-sm-6 col-lg-3">
+                    <div class="card border border-primary" style="background: rgba(3, 169, 244, 0.1);">
+                      <div class="card-body p-3 text-center">
+                        <!-- <div class="text-right text-green">
+                          6%
+                          <i class="fe fe-chevron-up"></i>
+                        </div> -->
+                        <div class="row">
+                          <div class="col-md-4">
+                            <div class="h1 m-0"><i class="fa fa-box"></i></div>
+                            <div class="text-muted mb-4"><?php echo $barang[$i*10];?></div>
+                          </div>
+                          <div class="col-md-4">
+                            <div class="h1 m-0"><i class="fa fa-box"></i></div>
+                            <div class="text-muted mb-4"><?php echo $barang[$i*10-1];?></div>
+                          </div>
+                          <div class="col-md-4">
+                            <div class="h1 m-0"><i class="fa fa-box"></i></div>
+                            <div class="text-muted mb-4"><?php echo $barang[$i*10+1];?></div>
+                          </div>
                         </div>
-                        <div class="col-md-4">
-                          <div class="h1 m-0"><i class="fa fa-box"></i></div>
-                          <div class="text-muted mb-4"><?php echo $barang[$i*10-1];?></div>
-                        </div>
-                        <div class="col-md-4">
-                          <div class="h1 m-0"><i class="fa fa-box"></i></div>
-                          <div class="text-muted mb-4"><?php echo $barang[$i*10+1];?></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="h1 m-0"><?php echo rand(60,100);?>%</div>
-                          <div class="text-muted">Ramalan keluar bersama</div>
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="h1 m-0"><?php echo rand(60,100);?>%</div>
+                            <div class="text-muted">Ramalan keluar bersama</div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              <?php } ?>
+                <?php } ?>
+              </div>
               
             </div>
           </div>
         </div>
       </div>
+      <script>
+        var myVar;
+        function myFunction() {
+          document.getElementById("ctn_loading").style.display = 'block';
+          document.getElementById("ctn_analisa").classList.add('d-none');
+            myVar = setTimeout(showPage, 700);
+        }
+        function showPage() {
+          document.getElementById("ctn_loading").style.display = 'none';
+          document.getElementById("ctn_analisa").classList.remove('d-none');
+        }
+        myFunction();
+      </script>
--- application/views/header.php
+++ application/views/header.php
@@ -12,18 +12,21 @@
     <title><?php echo isset($page_title)?$page_title:'D Kat Inventory';?></title>
     <link href="<?php echo base_url('/assets/vendors/fontawesome-5.1.1/css/all.css');?>" rel="stylesheet" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
-    <script src="<?php echo base_url(); ?>/assets/js/require.min.js"></script>
+    
+    <script src="<?php echo site_url(); ?>/assets/js/require.min.js"></script>
     <script>
       requirejs.config({
-          baseUrl: '.'
+          baseUrl: '<?php echo site_url(); ?>'
       });
     </script>
     <!-- Dashboard Core -->
-    <link href="<?php echo base_url(); ?>/assets/css/dashboard.css" rel="stylesheet" />
-    <script src="<?php echo base_url(); ?>/assets/js/dashboard.js"></script>
+    <link href="<?php echo site_url(); ?>/assets/css/dashboard.css" rel="stylesheet" />
+    <script src="<?php echo site_url(); ?>/assets/js/dashboard.js"></script>
+    
     <!-- c3.js Charts Plugin -->
-    <link href="<?php echo base_url(); ?>/assets/plugins/charts-c3/plugin.css" rel="stylesheet" />
-    <script src="<?php echo base_url(); ?>/assets/plugins/charts-c3/plugin.js"></script>
+    <link href="<?php echo site_url(); ?>/assets/plugins/charts-c3/plugin.css" rel="stylesheet" />
+    <script src="<?php echo site_url(); ?>/assets/plugins/charts-c3/plugin.js"></script>
+    <script src="<?php echo site_url(); ?>/assets/plugins/input-mask/plugin.js"></script>
   </head>
   <body class="">
     <div class="page">
@@ -37,16 +40,47 @@
                 D Kat Kategori
               </a>
               <div class="d-flex order-lg-2 ml-auto">
+                <div class="dropdown d-none d-md-flex">
+                  <a class="nav-link icon" data-toggle="dropdown">
+                    <i class="fe fe-bell"></i>
+                    <span class="nav-unread"></span>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                    <a href="#" class="dropdown-item d-flex">
+                      <span class="avatar mr-3 align-self-center" style="background-image: url(demo/faces/male/41.jpg)"></span>
+                      <div>
+                        <strong>Nathan</strong> pushed new commit: Fix page load performance issue.
+                        <div class="small text-muted">10 minutes ago</div>
+                      </div>
+                    </a>
+                    <a href="#" class="dropdown-item d-flex">
+                      <span class="avatar mr-3 align-self-center" style="background-image: url(demo/faces/female/1.jpg)"></span>
+                      <div>
+                        <strong>Alice</strong> started new task: Tabler UI design.
+                        <div class="small text-muted">1 hour ago</div>
+                      </div>
+                    </a>
+                    <a href="#" class="dropdown-item d-flex">
+                      <span class="avatar mr-3 align-self-center" style="background-image: url(demo/faces/female/18.jpg)"></span>
+                      <div>
+                        <strong>Rose</strong> deployed new version of NodeJS REST Api V3
+                        <div class="small text-muted">2 hours ago</div>
+                      </div>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item text-center text-muted-dark">Mark all as read</a>
+                  </div>
+                </div>
                 <div class="dropdown">
                   <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
                     <span class="avatar" style="background-image: url(<?php echo base_url('assets/images/person.png');?>)"></span>
                     <span class="ml-2 d-none d-lg-block">
-                      <span class="text-default">Jane Pearson</span>
+                      <span class="text-default"><?php if (isset($_SESSION['us_sess']) == 1) { echo $_SESSION['nm_sess']; } ?></span>
                       <small class="text-muted d-block mt-1">Administrator</small>
                     </span>
                   </a>
                   <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="<?php echo base_url('login/logout') ?>">
                       <i class="dropdown-icon fe fe-log-out"></i> Sign out
                     </a>
                   </div>
@@ -61,14 +95,16 @@
         <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
           <div class="container">
             <div class="row align-items-center">
-            
+              <!-- <div class="col-lg-3 ml-auto">
+                <a href="#">Stok <i class="badge badge-danger">4</i></a>
+              </div> -->
               <div class="col-lg order-lg-first">
                 <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
                   <li class="nav-item">
-                    <a href="analisis" class="nav-link <?php echo isset($_SESSION['menu'])=='analisis'?'active':'';?>"><i class="fa fa-chart-line"></i> Analisis</a>
+                    <a href="analisis" class="nav-link <?php echo isset($_SESSION['menu'])&&$_SESSION['menu']=='analisis'?'active':'';?>"><i class="fa fa-chart-line"></i> Analisis</a>
                   </li>
                   <li class="nav-item">
-                    <a href="<?php echo site_url('analisis');?>" class="nav-link"><i class="fe fe-box"></i> Rak</a>
+                    <a href="<?php echo base_url('gudang'); ?>" class="nav-link <?php echo isset($_SESSION['menu'])&&$_SESSION['menu']=='gudang'?'active':'';?>"><i class="fe fe-home"></i> Rak</a>
                   </li>
                 </ul>
               </div>
